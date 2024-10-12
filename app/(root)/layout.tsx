5c7d6b302b5e8ef25d7cb04facf2ab160886b9dc
7ipolito <allanhipolito66@gmail.com>
2024-10-12 12:23:36 -0300

    to add leftSidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,3 +1,4 @@
+import LeftSideBar from "@/components/shared/LeftSideBar";
 import Navbar from "@/components/shared/navbar/Navbar";
 import React from "react";
 /* eslint-disable tailwindcss/no-custom-classname */
@@ -7,10 +8,10 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
     <main className="background-light850_dark100 relative">
       <Navbar />
       <div className="flex">
+        <LeftSideBar />
         <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-36 max-md:pb-14 sm:px-14">
           <div className="max-auto w-full max-w-5xl">{children}</div>
         </section>
-        {/* <RightSidebar /> */}
       </div>
       {/* <Toaster /> */}
     </main>
