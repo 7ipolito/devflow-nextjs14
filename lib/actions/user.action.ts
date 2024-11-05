218c0bc566e2a1d2e9a3e579fd2167aa478b2761
7ipolito <allanhipolito66@gmail.com>
2024-11-04 23:10:08 -0300

    feat: to create webhook to sync clerk database with mongodb database

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -2,6 +2,13 @@
 
 import User from "@/database/user.model";
 import { connectToDatabase } from "../mongoose";
+import Question from "@/database/question.model";
+import {
+  CreateUserParams,
+  DeleteUserParams,
+  UpdateUserParams,
+} from "./shared.types";
+import { revalidatePath } from "next/cache";
 
 export async function getUserByID(params: any) {
   try {
@@ -12,5 +19,61 @@ export async function getUserByID(params: any) {
     return user;
   } catch (error) {
     console.log(error);
+  }
+}
+
+export async function createUser(userData: CreateUserParams) {
+  try {
+    connectToDatabase();
+    const newUser = await User.create(userData);
+
+    await User.findByIdAndUpdate(newUser._id, { $inc: { reputation: 50 } });
+
+    return newUser;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+export async function deleteUser(params: DeleteUserParams) {
+  try {
+    connectToDatabase();
+    const { clerkId } = params;
+    const user = await User.findOneAndDelete({ clerkId });
+    if (!user) {
+      throw new Error("User not found");
+    }
+    // Delete user from database
+    // and questions, answers, and comments, etc.
+
+    // get user questions ids
+    // const userQuestionsIds = await Question.find({ author: user._id }).distinct(
+    //   "_id",
+    // );
+
+    await Question.deleteMany({ author: user._id });
+
+    // TODO: delete user answers, comments, etc.
+    const deleteUser = await User.findByIdAndDelete(user._id);
+    return deleteUser;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+export async function updateUser(updatedData: UpdateUserParams) {
+  try {
+    connectToDatabase();
+    const { clerkId, path, updateData } = updatedData;
+    await User.findOneAndUpdate({ clerkId }, updateData, {
+      new: true,
+    });
+
+    revalidatePath(path);
+  } catch (error) {
+    console.log(error);
+    throw error;
   }
 }
