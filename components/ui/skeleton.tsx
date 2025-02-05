17a3803dbb5bf95493ade5a6d14c55ece92a749c
7ipolito <allanhipolito66@gmail.com>
2025-02-05 11:08:43 -0300

    fix: to fix a error

--- components/ui/skeleton.tsx
+++ components/ui/skeleton.tsx
@@ -1,5 +1,6 @@
-import { cn } from "@/lib/utils"
+import React from "react";
 
+import { cn } from "@/lib/utils";
 function Skeleton({
   className,
   ...props
@@ -9,7 +10,7 @@ function Skeleton({
       className={cn("animate-pulse rounded-md bg-muted", className)}
       {...props}
     />
-  )
+  );
 }
 
-export { Skeleton }
+export { Skeleton };
