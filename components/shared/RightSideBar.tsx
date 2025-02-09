c552cbe89f74b264c697b8b1d422803ae49c0eeb
7ipolito <allanhipolito66@gmail.com>
2025-02-08 23:56:12 -0300

    feat: to handle top questions and popular tgs with real data

--- components/shared/RightSideBar.tsx
+++ components/shared/RightSideBar.tsx
@@ -1,44 +1,20 @@
-import Link from "next/link";
-import React from "react";
 import Image from "next/image";
+import Link from "next/link";
 import RenderTag from "./RenderTag";
-const RightSideBar = () => {
-  const hotQuestions = [
-    { _id: "1", title: "How do I use express as a custim server in NextJS?" },
-    { _id: "2", title: "How do I use express as a custim server in NextJS?" },
-    { _id: "3", title: "How do I use express as a custim server in NextJS?" },
-    { _id: "4", title: "How do I use express as a custim server in NextJS?" },
-  ];
+import { getHotQuestions } from "@/lib/actions/question.action";
+import { getTopPopularTags } from "@/lib/actions/tag.actions";
 
-  const popularTags = [
-    {
-      _id: "1",
-      name: "Javscript",
-      totalQuestions: 5,
-    },
-    {
-      _id: "2",
-      name: "Rust",
-      totalQuestions: 5,
-    },
-    {
-      _id: "3",
-      name: "How do I use express as a custim server in NextJS?",
-      totalQuestions: 5,
-    },
-    {
-      _id: "4",
-      name: "How do I use express as a custim server in NextJS?",
-      totalQuestions: 5,
-    },
-  ];
+const RightSidebar = async () => {
+  const hotQuestions = await getHotQuestions();
+  const popularTags = await getTopPopularTags();
+
   return (
     <aside className="background-light900_dark200 light-border custom-scrollbar no-scrollbar sticky right-0 top-0 flex h-screen w-[350px] flex-col overflow-y-auto border-l p-6 pt-36 shadow-light-300 dark:shadow-none max-xl:hidden">
       {/* Top questions */}
       <div>
         <h3 className="h3-bold text-dark200_light900">Top Questions</h3>
         <div className="mt-7 flex w-full flex-col gap-[30px]">
-          {hotQuestions.map((question) => {
+          {hotQuestions.map((question: any) => {
             return (
               <Link
                 key={question._id}
@@ -71,7 +47,7 @@ const RightSideBar = () => {
                 key={tag._id}
                 _id={tag._id}
                 name={tag.name}
-                totalQuestions={tag.totalQuestions}
+                totalQuestions={tag.numberOfQuestions}
                 showCount
               />
             );
@@ -82,4 +58,4 @@ const RightSideBar = () => {
   );
 };
 
-export default RightSideBar;
+export default RightSidebar;
