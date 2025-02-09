3ea10e81fad30103002670620e68248dfa4ad869
7ipolito <allanhipolito66@gmail.com>
2025-02-09 10:36:52 -0300

    feat: to implements local search acordding with url

--- components/shared/search/LocalSearchbar.tsx
+++ components/shared/search/LocalSearchbar.tsx
@@ -2,8 +2,9 @@
 
 import Image from "next/image";
 import { Input } from "@/components/ui/input";
-import { useSearchParams } from "next/navigation";
-import { useState } from "react";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
+import { formUrlQuery, removeKeysFromQuery } from "@/lib/utils";
 // import { formUrlQuery, removeKeysFromQuery } from "@/lib/utils";
 
 interface Props {
@@ -21,44 +22,44 @@ const LocalSearchbar = ({
   placeholder,
   otherClasses,
 }: Props) => {
-  //   const router = useRouter();
-  //   const pathname = usePathname();
+  const router = useRouter();
+  const pathname = usePathname();
   const searchParams = useSearchParams();
 
   const query = searchParams.get("q");
 
   const [search, setSearch] = useState(query || "");
 
-  //   useEffect(() => {
-  //     /**
-  //      * we can't make a call to the server on each key stroke.
-  //      * ? Debounce
-  //      *
-  //      */
-  //     const delayDebounceFn = setTimeout(() => {
-  //       if (search) {
-  //         const newUrl = formUrlQuery({
-  //           params: searchParams.toString(),
-  //           key: "q",
-  //           value: search,
-  //         });
+  useEffect(() => {
+    /**
+     * we can't make a call to the server on each key stroke.
+     * ? Debounce
+     *
+     */
+    const delayDebounceFn = setTimeout(() => {
+      if (search) {
+        const newUrl = formUrlQuery({
+          params: searchParams.toString(),
+          key: "q",
+          value: search,
+        });
+        console.log(newUrl);
+        router.push(newUrl, { scroll: false });
+      } else {
+        // if the query doesn't exist
+        if (pathname === route) {
+          const newUrl = removeKeysFromQuery({
+            params: searchParams.toString(),
+            keysToRemove: ["q"],
+          });
 
-  //         router.push(newUrl, { scroll: false });
-  //       } else {
-  //         // if the query doesn't exist
-  //         if (pathname === route) {
-  //           const newUrl = removeKeysFromQuery({
-  //             params: searchParams.toString(),
-  //             keysToRemove: ["q"],
-  //           });
+          router.push(newUrl, { scroll: false });
+        }
+      }
+    }, 500);
 
-  //           router.push(newUrl, { scroll: false });
-  //         }
-  //       }
-  //     }, 500);
-
-  //     return () => clearTimeout(delayDebounceFn);
-  //   }, [search, route, pathname, router, searchParams, query]);
+    return () => clearTimeout(delayDebounceFn);
+  }, [search, route, pathname, router, searchParams, query]);
 
   return (
     <div
