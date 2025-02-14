56b6c7d1c9e4110b6038793f5f18ee55e36359f9
7ipolito <allanhipolito66@gmail.com>
2025-02-14 13:32:02 -0300

    feat: to implement reputation system

--- lib/actions/answer.action.ts
+++ lib/actions/answer.action.ts
@@ -5,9 +5,11 @@ import { connectToDatabase } from "../mongoose";
 import {
   AnswerVoteParams,
   CreateAnswerParams,
+  DeleteAnswerParams,
   GetAnswersParams,
 } from "./shared.types";
 import Question from "@/database/question.model";
+import Interaction from "@/database/interaction.model";
 import { revalidatePath } from "next/cache";
 import User from "@/database/user.model";
 
@@ -19,8 +21,18 @@ export async function createAnswer(params: CreateAnswerParams) {
 
     const newAnswer = await Answer.create({ content, author, question });
 
-    await Question.findByIdAndUpdate(question, {
+    // Add the answer to the question's answers array
+    const questionObject = await Question.findByIdAndUpdate(question, {
       $push: { answers: newAnswer._id },
+    });
+
+    // Increment user reputation
+    await Interaction.create({
+      user: author,
+      action: "answer",
+      question,
+      answer: newAnswer.id,
+      tags: questionObject.tags,
     });
 
     await User.findByIdAndUpdate(author, { $inc: { reputation: 10 } });
@@ -35,15 +47,45 @@ export async function createAnswer(params: CreateAnswerParams) {
 export async function getAnswers(params: GetAnswersParams) {
   try {
     connectToDatabase();
-    const { questionId, page = 1, pageSize = 10 } = params;
+    const { questionId, sortBy, page = 1, pageSize = 10 } = params;
 
+    // for Pagination => caluclate the number of posts to skip based on the pageNumber and pageSize
     const skipAmount = (page - 1) * pageSize;
+
+    /**
+     * Sorting
+     */
+    let sortOptions = {};
+    switch (sortBy) {
+      case "higestUpvotes":
+        sortOptions = { upvotes: -1 };
+        break;
+
+      case "lowestUpvotes":
+        sortOptions = { upvotes: 1 };
+        break;
+
+      case "recent":
+        sortOptions = { createdAt: -1 };
+        break;
+
+      case "old":
+        sortOptions = { createdAt: 1 };
+        break;
+
+      default:
+        break;
+    }
 
     const answers = await Answer.find({ question: questionId })
       .populate("author", "_id clerkId name picture")
       .skip(skipAmount)
-      .limit(pageSize);
+      .limit(pageSize)
+      .sort(sortOptions);
 
+    /**
+     * Pagination
+     */
     const totalAnswers = await Answer.countDocuments({
       question: questionId,
     });
@@ -134,6 +176,38 @@ export async function downvoteAnswer(params: AnswerVoteParams) {
     await User.findByIdAndUpdate(answer.author, {
       $inc: { reputation: hasDownvoted ? -10 : 10 },
     });
+
+    revalidatePath(path);
+  } catch (error) {
+    console.error(`❌ ${error} ❌`);
+    throw error;
+  }
+}
+
+export async function deleteAnswer(params: DeleteAnswerParams) {
+  try {
+    connectToDatabase();
+
+    const { answerId, path } = params;
+
+    // Find the answer
+    const answer = await Answer.findById(answerId);
+
+    if (!answer) {
+      throw new Error(`❌ Answer not found ❌`);
+    }
+
+    // Delete the answer
+    await answer.deleteOne({ _id: answerId });
+
+    // Update all question that include the answer
+    await Question.updateMany(
+      { _id: answer.question },
+      { $pull: { answers: answerId } }
+    );
+
+    // Delete all interaction relative to the answer
+    await Interaction.deleteMany({ answer: answerId });
 
     revalidatePath(path);
   } catch (error) {
